2852fd06be1235445eef7563d4537bf8e35ce5d0
shadrack.adwera <adweshshaddie@gmail.com>
2021-06-02 06:38:56 +0300

    update POST controller to use sequelize

--- controllers/feed-controllers.js
+++ controllers/feed-controllers.js
@@ -1,4 +1,4 @@
-const db = require('../db/db');
+const Feed = require('../models/feed-model');
 
 const posts = async(req,res,next) => {
     
@@ -6,8 +6,14 @@ const posts = async(req,res,next) => {
 
 const newPost = async(req,res,next) => {
     const { title, description } = req.body;
-    
-    
+    try {
+        const result = await Feed.create({
+            title, description
+        });
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
     res.status(201).json({message: 'Post Created'})
 }
 
