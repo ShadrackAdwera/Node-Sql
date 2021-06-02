d4860372aa8354a43001418bee39ace770a8dcfd
shadrack.adwera <adweshshaddie@gmail.com>
2021-06-02 05:49:11 +0300

    create feed model

---
+++ models/feed-model.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize';
+
+const sequelize = require('../db/db');
+
+const Feed = sequelize.define('feed',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: true,
+    createdAt: true,
+    updatedAt: true
+})
+
+module.exports = Feed;
